02d98d6c3231cfefc88c4f5f9f528d4e3a05a8fd
MarcoPicuno <76054145+MarcoPicuno@users.noreply.github.com>
2021-02-15 11:07:30 +0100

    Add files via upload

---
+++ Angular_CB/Carbook/src/app/pagina-alert/pagina-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-alert',
+  templateUrl: './pagina-alert.component.html',
+  styleUrls: ['./pagina-alert.component.css']
+})
+export class PaginaAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
