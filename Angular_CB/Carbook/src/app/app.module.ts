fe3f51ddd37447f96d4bff7ee5cd17bbeb16759e
AzureAD\LucaGiovanniNastasi <l.nastasi@itsrizzoli.it>
2021-02-16 19:16:38 +0100

    no message

--- Angular_CB/Carbook/src/app/app.module.ts
+++ Angular_CB/Carbook/src/app/app.module.ts
@@ -1,5 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PrimapaginaComponent } from './primapagina/primapagina.component';
+import { DirettiveComponent } from './direttive/direttive.component';
+import { PasswordDimenticataComponent } from './password-dimenticata/password-dimenticata.component';
+import { PaginaAlertComponent } from './pagina-alert/pagina-alert.component';
+import { ReimpostaPasswordComponent } from './reimposta-password/reimposta-password.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    PrimapaginaComponent,
+    DirettiveComponent,
+    PasswordDimenticataComponent,
+    PaginaAlertComponent,
+    ReimpostaPasswordComponent
+  ],
+  imports: [
+      BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
+})
+export class AppModule { }
+
+/*
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,3 +55,4 @@ import { DirettiveComponent } from './direttive/direttive.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+*/
