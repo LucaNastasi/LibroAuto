b17764ca92cf9011ea1664166d35901fe1ea9f0d
MarcoPicuno <76054145+MarcoPicuno@users.noreply.github.com>
2021-02-18 15:26:42 +0100

    Add files via upload

---
+++ Angular_CB/Carbook/src/app/p-utente/p-utente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-p-utente',
+  templateUrl: './p-utente.component.html',
+  styleUrls: ['./p-utente.component.css']
+})
+export class PUtenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
