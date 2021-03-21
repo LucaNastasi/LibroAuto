14d6ec7571afd4fcd2e77635460395ac58f4129e
MarcoPicuno <76054145+MarcoPicuno@users.noreply.github.com>
2021-03-21 18:27:07 +0100

    Add files via upload

---
+++ Angular_CB/Carbook/src/app/visione-annuncio/visione-annuncio.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visione-annuncio',
+  templateUrl: './visione-annuncio.component.html',
+  styleUrls: ['./visione-annuncio.component.css']
+})
+export class VisioneAnnuncioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
